40f821a730be6a1a0fa0c11fdc2d3a4f2194ad24
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-13 14:38:21 -0400

    dev'd main to read four int inputs, stores int in int array; print for testing

--- SwappingVariables.java
+++ SwappingVariables.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class SwappingVariables {
    
-   /* Define your method here */
    public static void swapValues(int[] intArray){
     int tempVal = intArray[0];
     intArray[0] = intArray[1];
@@ -16,6 +15,14 @@ public class SwappingVariables {
    }
    
    public static void main(String[] args) {
-      /* Type your code here. */
+      Scanner scnr = new Scanner(System.in);
+      final int NUM_ELEMENTS = 4;
+      int[] values = new int[NUM_ELEMENTS];
+
+      for(int i = 0; i < values.length; ++i){
+        values[i] = scnr.nextInt();
+        System.out.println(values[i]);
+      }
+      scnr.close();
    }
 }
