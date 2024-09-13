43c40e7d1ee442c1a363df1beedfb560204f5497
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-13 14:35:15 -0400

    defined method to handling swapping element indices

--- SwappingVariables.java
+++ SwappingVariables.java
@@ -5,6 +5,15 @@ import java.util.Scanner;
 public class SwappingVariables {
    
    /* Define your method here */
+   public static void swapValues(int[] intArray){
+    int tempVal = intArray[0];
+    intArray[0] = intArray[1];
+    intArray[1] = tempVal;
+
+    tempVal = intArray[2];
+    intArray[2] = intArray[3];
+    intArray[3] = tempVal;
+   }
    
    public static void main(String[] args) {
       /* Type your code here. */
